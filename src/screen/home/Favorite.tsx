7e4de1971dbf3f2e527a934e198659c2ccfa24af
nqam.momo98 <minh.nghiem@mservice.com.vn>
2023-05-08 15:52:48 +0700

    fix lint

--- src/screen/home/Favorite.tsx
+++ src/screen/home/Favorite.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { dataDetailCategory } from 'common/data';
