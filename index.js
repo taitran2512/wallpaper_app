8f02022cda1f8c1a99bb12f39b6940cfaa76d91a
nqam.momo98 <minh.nghiem@mservice.com.vn>
2023-06-02 15:54:07 +0700

    change key dev

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { AppRegistry, LogBox, Text } from 'react-native';
 import { AdManager } from 'react-native-admob-native-ads';
 import { name as appName } from './app.json';
 import App from './src/App';
-import { objScreen, Screens } from './src/common';
+import { Screens, objScreen } from './src/common';
 // get screen name from import
 for (let key in objScreen) {
 	Screens[key] = key;
@@ -19,7 +19,7 @@ remoteConfig();
 // image test ad
 AdManager.registerRepository({
 	name: 'imageAd',
-	adUnitId: 'ca-app-pub-6548166688052880/8131685939',
+	adUnitId: 'ca-app-pub-3940256099942544/2247696110',
 	numOfAds: 3,
 	nonPersonalizedAdsOnly: false,
 	videoOptions: {
--- src/utils/GoogleAds.ts
+++ src/utils/GoogleAds.ts
@@ -1,32 +1,34 @@
+import { TestIds } from 'react-native-google-mobile-ads';
+
 // KEY DEV
-// export const keyAdsOpenApp: string | any = TestIds.APP_OPEN;
-// export const keyBanner_onboarding: string | any = TestIds.BANNER;
-// export const keyNative_onboarding: string | any = 'ca-app-pub-3940256099942544/2247696110';
-// export const keyBanner_home: string | any = TestIds.BANNER;
-// export const keyBanner_category: string | any = TestIds.BANNER;
-// export const keyInterstitialSplash: string | any = TestIds.INTERSTITIAL;
-// export const keyInterstitialApplyHigh: string | any = TestIds.INTERSTITIAL;
-// export const keyInterstitialApply: string | any = TestIds.INTERSTITIAL;
-// export const keyInterstitialOpenImageHigh: string = TestIds.INTERSTITIAL;
-// export const keyInterstitialOpenImage: string = TestIds.INTERSTITIAL;
-// export const keyInterstitialOpenCateHigh: string = TestIds.INTERSTITIAL;
-// export const keyInterstitialOpenCate: string = TestIds.INTERSTITIAL;
-// export const keyOnAppResume: string | any = TestIds.APP_OPEN;
+export const keyAdsOpenApp: string | any = TestIds.APP_OPEN;
+export const keyBanner_onboarding: string | any = TestIds.BANNER;
+export const keyNative_onboarding: string | any = 'ca-app-pub-3940256099942544/2247696110';
+export const keyBanner_home: string | any = TestIds.BANNER;
+export const keyBanner_category: string | any = TestIds.BANNER;
+export const keyInterstitialSplash: string | any = TestIds.INTERSTITIAL;
+export const keyInterstitialApplyHigh: string | any = TestIds.INTERSTITIAL;
+export const keyInterstitialApply: string | any = TestIds.INTERSTITIAL;
+export const keyInterstitialOpenImageHigh: string = TestIds.INTERSTITIAL;
+export const keyInterstitialOpenImage: string = TestIds.INTERSTITIAL;
+export const keyInterstitialOpenCateHigh: string = TestIds.INTERSTITIAL;
+export const keyInterstitialOpenCate: string = TestIds.INTERSTITIAL;
+export const keyOnAppResume: string | any = TestIds.APP_OPEN;
 
 /// Key PRODUCTION
-export const keyAdsOpenApp: string | any = 'ca-app-pub-6548166688052880/4282287871';
-export const keyBanner_onboarding: string | any = 'ca-app-pub-6548166688052880/8900189737';
-export const keyNative_onboarding: string | any = 'ca-app-pub-6548166688052880/8131685939';
-export const keyBanner_home: string | any = 'ca-app-pub-6548166688052880/8029961195';
-export const keyBanner_category: string | any = 'ca-app-pub-6548166688052880/3647863050';
-export const keyInterstitialSplash = 'ca-app-pub-6548166688052880/9444767609';
-export const keyInterstitialApplyHigh = 'ca-app-pub-6548166688052880/5999280959';
-export const keyInterstitialApply = 'ca-app-pub-6548166688052880/1021699710';
-export const keyInterstitialOpenImageHigh: string = 'ca-app-pub-6548166688052880/1368359669';
-export const keyInterstitialOpenImage: string = 'ca-app-pub-6548166688052880/3994523008';
-export const keyInterstitialOpenCateHigh: string = 'ca-app-pub-6548166688052880/9598999633';
-export const keyInterstitialOpenCate: string = 'ca-app-pub-6548166688052880/5738849518';
-export const keyOnAppResume = 'ca-app-pub-6548166688052880/5721145011';
+// export const keyAdsOpenApp: string | any = 'ca-app-pub-6548166688052880/4282287871';
+// export const keyBanner_onboarding: string | any = 'ca-app-pub-6548166688052880/8900189737';
+// export const keyNative_onboarding: string | any = 'ca-app-pub-6548166688052880/8131685939';
+// export const keyBanner_home: string | any = 'ca-app-pub-6548166688052880/8029961195';
+// export const keyBanner_category: string | any = 'ca-app-pub-6548166688052880/3647863050';
+// export const keyInterstitialSplash = 'ca-app-pub-6548166688052880/9444767609';
+// export const keyInterstitialApplyHigh = 'ca-app-pub-6548166688052880/5999280959';
+// export const keyInterstitialApply = 'ca-app-pub-6548166688052880/1021699710';
+// export const keyInterstitialOpenImageHigh: string = 'ca-app-pub-6548166688052880/1368359669';
+// export const keyInterstitialOpenImage: string = 'ca-app-pub-6548166688052880/3994523008';
+// export const keyInterstitialOpenCateHigh: string = 'ca-app-pub-6548166688052880/9598999633';
+// export const keyInterstitialOpenCate: string = 'ca-app-pub-6548166688052880/5738849518';
+// export const keyOnAppResume = 'ca-app-pub-6548166688052880/5721145011';
 
 // LINK WEB
 export const urlPolicy: string =
